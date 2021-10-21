857e50119c0a5ff755e158735f720005c96a40dd
Mariamgad <mariamgad142@gmail.com>
2021-10-21 22:17:14 +0200

    registering a screen as the main screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,20 +7,25 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map(
-            (catDate) => CategoryItem(
-              title: catDate.title,
-              color: catDate.color,
-            ),
-          )
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal app'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catDate) => CategoryItem(
+                title: catDate.title,
+                color: catDate.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,21 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
 
